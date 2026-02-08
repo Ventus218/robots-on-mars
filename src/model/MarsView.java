9cbff463c505c45c95d83c7408106cf5c2e16d59
Alessandro Venturini <ales.ventus21@gmail.com>
2026-02-08 14:32:27 +0100

    made MarsView synchronized

--- src/model/MarsView.java
+++ src/model/MarsView.java
@@ -8,14 +8,14 @@ public class MarsView {
 
     private final Map<Coordinates, TerrainView> view = new MapWithDefault<>(new TerrainView.Unknown());
 
-    public Map<Coordinates, TerrainView.Known> knownTerrain() {
+    synchronized public Map<Coordinates, TerrainView.Known> knownTerrain() {
         return view.entrySet()
                 .stream()
                 .filter(entry -> entry.getValue() instanceof TerrainView.Known)
                 .collect(Collectors.toMap(e -> e.getKey(), e -> (TerrainView.Known) e.getValue()));
     }
 
-    public void updateView(Map<Coordinates, TerrainView.Known> update) {
+    synchronized public void updateView(Map<Coordinates, TerrainView.Known> update) {
         update.entrySet().stream().forEach(e -> view.put(e.getKey(), e.getValue()));
     }
 }
