ae68c35fee71b623b6342932de2586823e1289b8
Alessandro Venturini <ales.ventus21@gmail.com>
2026-02-10 18:16:10 +0100

    update config to production

--- src/model/Config.java
+++ src/model/Config.java
@@ -7,12 +7,12 @@ public final class Config {
     public static final float MARS_MINING_SPOTS_DENSITY = 0.01f;
     public static final int MARS_BASE_SIZE = 3;
     public static final int MARS_BASE_ANTENNA_RANGE = 7;
-    public static final int ROVER_BATTERY_CAPACITY = 50;
+    public static final int ROVER_BATTERY_CAPACITY = 100;
     public static final int ROVER_CAMERA_RANGE = 3;
     public static final int ROVER_ANTENNA_RANGE = 5;
-    public static final int SCIENTIST_SAMPLES_CAPACITY = 3;
+    public static final int SCIENTIST_SAMPLES_CAPACITY = 4;
     public static final int SCIENTIST_MINING_SAMPLE_ENERGY_COST = 10;
-    public static final int MOVEMENT_DURATION_MS = 500;
+    public static final int MOVEMENT_DURATION_MS = 1000;
     public static final int RECHARGE_DURATION_MS = 2000;
     public static final int MINE_SAMPLE_DURATION_MS = 3000;
     public static final int COLLECT_SAMPLE_DURATION_MS = 2000;
